e53054a20ff3ae491dcb0fe128c8ec31eabd6d07
bvUCONN <143756787+bvUCONN@users.noreply.github.com>
2023-12-05 23:42:43 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = "doodles";
+
+const cacheEm = [
+  "index.html",
+  "sketch.js",
+  "script.js",
+  "style.css",
+  "/",
+  "https://cdn.jsdelivr.net/npm/p5@1.8.0/lib/p5.js",
+  "https://kit.fontawesome.com/df98c7d392.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cacheEm);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Browser is requesting", event.request);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    }),
+  );
+});
